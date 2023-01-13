477352808a42ba52842bc9c79df8a5762facb202
Fynn Ante <fynn.ante@outlook.de>
2023-01-13 10:08:29 +0100

    Designanpassung, Filter auf Dateiname eingebaut, Gesperrt Spalte eingebaut

--- src/main/java/de/vonraesfeld/manhart/aldenkirchs/application/views/main/TabellenView.java
+++ src/main/java/de/vonraesfeld/manhart/aldenkirchs/application/views/main/TabellenView.java
@@ -1,14 +1,16 @@
 package de.vonraesfeld.manhart.aldenkirchs.application.views.main;
 
-import com.vaadin.flow.component.grid.Grid;
-import de.vonraesfeld.manhart.aldenkirchs.application.daos.DateiVersionDao;
-import de.vonraesfeld.manhart.aldenkirchs.application.entities.DateiVersion;
 import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.grid.Grid;
+import com.vaadin.flow.component.html.Span;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextField;
-import com.vaadin.flow.component.treegrid.TreeGrid;
+import com.vaadin.flow.data.renderer.ComponentRenderer;
 import com.vaadin.flow.data.value.ValueChangeMode;
+import com.vaadin.flow.function.SerializableBiConsumer;
+import de.vonraesfeld.manhart.aldenkirchs.application.daos.DateiVersionDao;
+import de.vonraesfeld.manhart.aldenkirchs.application.entities.DateiVersion;
 import java.util.Date;
 import org.springframework.stereotype.Component;
 
@@ -16,12 +18,15 @@ import org.springframework.stereotype.Component;
 public class TabellenView extends VerticalLayout {
 
   private final DateiVersionDao dateiVersionDao;
+  private final VersionsverwaltungService versionsverwaltungService;
   private final MainView mainView;
   Grid<DateiVersion> grid;
 
 
-  public TabellenView(DateiVersionDao dateiVersionDao, MainView mainView) {
+  public TabellenView(DateiVersionDao dateiVersionDao,
+                      VersionsverwaltungService versionsverwaltungService, MainView mainView) {
     this.dateiVersionDao = dateiVersionDao;
+    this.versionsverwaltungService = versionsverwaltungService;
     this.mainView = mainView;
     setSizeFull();
     configureGrid();
@@ -29,16 +34,38 @@ public class TabellenView extends VerticalLayout {
   }
 
   private void configureGrid() {
-    grid = new Grid<>(DateiVersion.class);
+    grid = new Grid<>(DateiVersion.class, false);
     grid.setSizeFull();
     grid.addClassNames("dateiversion-grid");
     grid.asSingleSelect().addValueChangeListener(e -> editDateiVersion(e.getValue()));
+    grid.addColumn(DateiVersion::getDateityp).setHeader("Typ").setWidth("2em").setSortable(true);
+    grid.addColumn(DateiVersion::getDateiname).setHeader("Dateiname").setAutoWidth(true)
+        .setSortable(true);
+    grid.addColumn(DateiVersion::getVersion).setHeader("Version").setWidth("2em").setSortable(true);
+    grid.addColumn(createGesperrtSpalte()).setHeader("Status").setAutoWidth(true).setSortable(true);
+    grid.addColumn(DateiVersion::getZuletztBearbeitet).setHeader("Zuletzt Bearbeitet")
+        .setAutoWidth(true).setSortable(true);
+    grid.addColumn(DateiVersion::getErstelltAm).setHeader("Erstellt am").setAutoWidth(true)
+        .setSortable(true);
     grid.getColumns().forEach(col -> col.setAutoWidth(true));
     updateList();
   }
 
+  private ComponentRenderer<Span, DateiVersion> createGesperrtSpalte() {
+    return new ComponentRenderer<>(Span::new, statusComponentUpdater);
+  }
+
+  private SerializableBiConsumer<Span, DateiVersion> statusComponentUpdater = (
+      span, dateiVersion) -> {
+    Boolean gesperrt = dateiVersion.getGesperrt();
+    String theme = String.format("badge %s",
+        Boolean.FALSE.equals(gesperrt) ? "error" : "success");
+    span.getElement().setAttribute("theme", theme);
+    span.setText(Boolean.FALSE.equals(gesperrt) ? "Gesperrt" : "Verfügbar");
+  };
+
   private void editDateiVersion(DateiVersion value) {
-    if (value ==  null){
+    if (value == null) {
       mainView.closeEditor();
     } else {
       mainView.getDateiVersionEditPanel().setDateiVersion(value);
@@ -51,16 +78,20 @@ public class TabellenView extends VerticalLayout {
   private HorizontalLayout getToolbar() {
     final TextField filterText = new TextField();
     filterText.setPlaceholder("Nach Dateiname filtern...");
+    filterText.setWidth("13em");
     //Button, der Filter wieder löscht
     filterText.setClearButtonVisible(true);
     //feuert nicht bei jedem Change neue Datenbankabfrage
     filterText.setValueChangeMode(ValueChangeMode.LAZY);
-    filterText.addValueChangeListener(e -> updateList());
+    filterText.addValueChangeListener(
+        e -> grid.setItems(versionsverwaltungService.findByDateiname(filterText.getValue())));
 
     final Button addDateiVersionButton = new Button("Datei hinzufügen");
     addDateiVersionButton.addClickListener(e -> addDateiVersion());
 
     final HorizontalLayout toolbar = new HorizontalLayout(filterText, addDateiVersionButton);
+    toolbar.setJustifyContentMode(JustifyContentMode.BETWEEN);
+    toolbar.setWidthFull();
     toolbar.addClassName("toolbar");
     return toolbar;
   }
